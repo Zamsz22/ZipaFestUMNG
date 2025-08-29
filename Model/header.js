444e386c452060c21bc80f922ffb935612983295
Zamsz22 <davidsambr716@gmail.com>
2025-08-29 16:55:52 -0500

    Fix Hyperlinks
    Todas los sub html

---
+++ Model/header.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('header');
+const scrollDistance = 25;
+window.addEventListener('scroll', () => {
+    if (window.scrollY > scrollDistance) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
